56f743b2d4424d3082f42172467033b326053ded
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2020-07-08 13:54:09 -0400

    SWDEV-231662 update to trunk
    This change is required for building AMDMIGraphX for P1 ticket SWDEV-231662.
    
    This patch update the change to latest version in trunk
    
    Differential Revision: https://reviews.llvm.org/D78655
    
    Also cherry-picks 9275e14379961a4304de559f16fdbac275fb6301
    
    Differential Revision: https://reviews.llvm.org/D83893
    
    Change-Id: I9583792a0a41ad3117a02c68d7d4df2a253c0f71

--- clang/lib/Sema/SemaLambda.cpp
+++ clang/lib/Sema/SemaLambda.cpp
@@ -993,10 +993,9 @@ void Sema::ActOnStartOfLambdaDefinition(LambdaIntroducer &Intro,
   // Attributes on the lambda apply to the method.
   ProcessDeclAttributes(CurScope, Method, ParamInfo);
 
-  // CUDA lambdas get implicit attributes based on the scope in which they're
-  // declared.
+  // CUDA lambdas get implicit host and device attributes.
   if (getLangOpts().CUDA)
-    CUDASetLambdaAttrs(Method, Intro);
+    CUDASetLambdaAttrs(Method);
 
   // Number the lambda for linkage purposes if necessary.
   handleLambdaNumbering(Class, Method);
@@ -1783,6 +1782,9 @@ ExprResult Sema::BuildLambdaExpr(SourceLocation StartLoc, SourceLocation EndLoc,
       BuildCaptureField(Class, From);
       Captures.push_back(Capture);
       CaptureInits.push_back(Init.get());
+
+      if (LangOpts.CUDA)
+        CUDACheckLambdaCapture(CallOperator, From);
     }
 
     // C++11 [expr.prim.lambda]p6:
--- clang/test/SemaCUDA/lambda.cu
+++ clang/test/SemaCUDA/lambda.cu
@@ -1,38 +1,73 @@
-// RUN: %clang_cc1 -std=c++17 -fsyntax-only -x hip -verify %s -fhip-lambda-host-device
+// RUN: %clang_cc1 -std=c++17 -fsyntax-only -verify=com %s
+// RUN: %clang_cc1 -std=c++17 -fsyntax-only -fcuda-is-device -verify=com,dev %s
 
 #include "Inputs/cuda.h"
 
+auto global_lambda = [] () { return 123; };
+
 template<class F>
 __global__ void kernel(F f) { f(); }
-// expected-error@-1 3{{no matching function for call to object of type}}
+// dev-note@-1 7{{called by 'kernel<(lambda}}
 
-constexpr __host__ __device__ void hd();
+__host__ __device__ void hd(int x);
+
+class A {
+  int b;
+public:
+  void test() {
+    [=](){ hd(b); }();
+
+    [&](){ hd(b); }();
+
+    kernel<<<1,1>>>([](){ hd(0); });
+
+    kernel<<<1,1>>>([=](){ hd(b); });
+    // dev-error@-1 {{capture host side class data member by this pointer in device or host device lambda function}}
+
+    kernel<<<1,1>>>([&](){ hd(b); });
+    // dev-error@-1 {{capture host side class data member by this pointer in device or host device lambda function}}
+
+    kernel<<<1,1>>>([&] __device__ (){ hd(b); });
+    // dev-error@-1 {{capture host side class data member by this pointer in device or host device lambda function}}
+
+    kernel<<<1,1>>>([&](){
+      auto f = [&]{ hd(b); };
+      // dev-error@-1 {{capture host side class data member by this pointer in device or host device lambda function}}
+      f();
+    });
+  }
+};
 
 int main(void) {
   auto lambda_kernel = [&]__global__(){};
-  // expected-error@-1 {{kernel function 'operator()' must be a free function or static member function}}
+  // com-error@-1 {{kernel function 'operator()' must be a free function or static member function}}
 
   int b;
+  [&](){ hd(b); }();
 
-  kernel<<<1,1>>>([](){ hd(); });
+  [=, &b](){ hd(b); }();
 
-  kernel<<<1,1>>>([=](){ hd(); });
+  kernel<<<1,1>>>(global_lambda);
 
-  kernel<<<1,1>>>([b](){ hd(); });
+  kernel<<<1,1>>>([](){ hd(0); });
 
-  kernel<<<1,1>>>([&]()constexpr{ hd(); });
+  kernel<<<1,1>>>([=](){ hd(b); });
 
-  kernel<<<1,1>>>([&](){ hd(); });
-  // expected-note@-1 {{in instantiation of function template specialization 'kernel<(lambda at}}
-  // expected-note@-2 {{candidate function not viable: call to __host__ function from __global__ function}}
+  kernel<<<1,1>>>([b](){ hd(b); });
 
-  kernel<<<1,1>>>([=, &b](){ hd(); });
-  // expected-note@-1 {{in instantiation of function template specialization 'kernel<(lambda at}}
-  // expected-note@-2 {{candidate function not viable: call to __host__ function from __global__ function}}
+  kernel<<<1,1>>>([&](){ hd(b); });
+  // dev-error@-1 {{capture host variable 'b' by reference in device or host device lambda function}}
 
-  kernel<<<1,1>>>([&, b](){ hd(); });
-  // expected-note@-1 {{in instantiation of function template specialization 'kernel<(lambda at}}
-  // expected-note@-2 {{candidate function not viable: call to __host__ function from __global__ function}}
+  kernel<<<1,1>>>([=, &b](){ hd(b); });
+  // dev-error@-1 {{capture host variable 'b' by reference in device or host device lambda function}}
+
+  kernel<<<1,1>>>([&, b](){ hd(b); });
+
+  kernel<<<1,1>>>([&](){
+      auto f = [&]{ hd(b); };
+      // dev-error@-1 {{capture host variable 'b' by reference in device or host device lambda function}}
+      f();
+  });
 
   return 0;
 }
