a7bb74d78cd9eee8d68762ad59ddd939831dccc5
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2020-08-24 18:05:34 -0400

    Revert "improve kernel launch latency"
    This reverts commit 3176cda999f0542f6cbfcf2466a5aac4af260073.

--- clang/test/CodeGenCUDA/kernel-call.cu
+++ clang/test/CodeGenCUDA/kernel-call.cu
@@ -1,19 +1,19 @@
 // RUN: %clang_cc1 -target-sdk-version=8.0 -emit-llvm %s -o - \
-// RUN: | FileCheck %s --check-prefixes=CUDA-OLD,CHECK,COMMON
+// RUN: | FileCheck %s --check-prefixes=CUDA-OLD,CHECK
 // RUN: %clang_cc1 -target-sdk-version=9.2  -emit-llvm %s -o - \
-// RUN: | FileCheck %s --check-prefixes=CUDA-NEW,CHECK,COMMON
+// RUN: | FileCheck %s --check-prefixes=CUDA-NEW,CHECK
 // RUN: %clang_cc1 -x hip -emit-llvm %s -o - \
-// RUN: | FileCheck %s --check-prefixes=HIP-OLD,CHECK,COMMON
+// RUN: | FileCheck %s --check-prefixes=HIP-OLD,CHECK
 // RUN: %clang_cc1 -fhip-new-launch-api -x hip -emit-llvm %s -o - \
-// RUN: | FileCheck %s --check-prefixes=HIP-NEW
+// RUN: | FileCheck %s --check-prefixes=HIP-NEW,CHECK
 
 #include "Inputs/cuda.h"
 
 // CHECK-LABEL: define{{.*}}g1
 // HIP-OLD: call{{.*}}hipSetupArgument
 // HIP-OLD: call{{.*}}hipLaunchByPtr
-// HIP-NEW-NOT: call{{.*}}__hipPopCallConfiguration
-// HIP-NEW-NOT: call{{.*}}hipLaunchKernel
+// HIP-NEW: call{{.*}}__hipPopCallConfiguration
+// HIP-NEW: call{{.*}}hipLaunchKernel
 // CUDA-OLD: call{{.*}}cudaSetupArgument
 // CUDA-OLD: call{{.*}}cudaLaunch
 // CUDA-NEW: call{{.*}}__cudaPopCallConfiguration
@@ -23,12 +23,11 @@ __global__ void g1(int x) {}
 // CHECK-LABEL: define{{.*}}main
 int main(void) {
   // HIP-OLD: call{{.*}}hipConfigureCall
-  // HIP-NEW-NOT: call{{.*}}__hipPushCallConfiguration
-  // HIP-NEW: call{{.*}}hipLaunchKernel
+  // HIP-NEW: call{{.*}}__hipPushCallConfiguration
   // CUDA-OLD: call{{.*}}cudaConfigureCall
   // CUDA-NEW: call{{.*}}__cudaPushCallConfiguration
-  // COMMON: icmp
-  // COMMON: br
-  // COMMON: call{{.*}}g1
+  // CHECK: icmp
+  // CHECK: br
+  // CHECK: call{{.*}}g1
   g1<<<1, 1>>>(42);
 }
