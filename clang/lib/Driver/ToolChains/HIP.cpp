887080fb89e69931f99ef1ab46fab966c77bb4f7
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2020-09-29 20:23:03 -0400

    Diagnose invalid target ID for assembler
    Change-Id: If23f79b46d90c76ae146b146bc359eff11447b4e

--- clang/lib/Driver/ToolChains/HIP.cpp
+++ clang/lib/Driver/ToolChains/HIP.cpp
@@ -235,8 +235,7 @@ void HIPToolChain::addClangTargetOptions(
     Action::OffloadKind DeviceOffloadingKind) const {
   HostTC.addClangTargetOptions(DriverArgs, CC1Args, DeviceOffloadingKind);
 
-  // Allow using target ID in --offload-arch.
-  StringRef GpuArch = translateTargetID(DriverArgs, CC1Args);
+  StringRef GpuArch = getGPUArch(DriverArgs);
   assert(!GpuArch.empty() && "Must have an explicit GPU arch.");
   (void) GpuArch;
   assert(DeviceOffloadingKind == Action::OFK_HIP &&
@@ -351,6 +350,7 @@ HIPToolChain::TranslateArgs(const llvm::opt::DerivedArgList &Args,
     checkTargetID(*DAL);
   }
 
+  checkTargetID(*DAL);
   return DAL;
 }
 
