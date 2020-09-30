26a12c9bbfead0b171a55d88bdd673ef6a156fdb
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2020-09-29 20:16:32 -0400

    Fix bound arch for offload action
    Change-Id: I6c1884a2f31ea76e2f846f222df0ac2401b20967

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -2517,7 +2517,7 @@ class OffloadingActionBuilder final {
 
       // If we have a fat binary, add it to the list.
       if (CudaFatBinary) {
-        AddTopLevel(CudaFatBinary, CudaArch::UNKNOWN);
+        AddTopLevel(CudaFatBinary, nullptr);
         CudaDeviceActions.clear();
         CudaFatBinary = nullptr;
         return;
