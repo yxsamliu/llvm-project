d241f60d97063fae4b87962d1ced0042cae8c2d3
Konstantin Zhuravlyov <konstantin.zhuravlyov@amd.com>
2020-10-20 18:52:15 -0400

    Revert "[AMDGPU][OPENMP] OpenMP AMDGCN Header Support"
    This reverts commit d5efeb345a5dd382a26924774e101a4ca3fe5306.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Ic885422347e0e9ddb69d7669c3f8cf1401a8da67

--- clang/lib/Headers/__clang_cuda_math.h
+++ clang/lib/Headers/__clang_cuda_math.h
@@ -26,19 +26,9 @@
 #if defined(__cplusplus)
 #define __DEVICE__ static constexpr __attribute__((always_inline, nothrow))
 #else
-// Use __BUILD_MATH_BUILTINS_LIB__ to build device specific libm-nvptx.bc
-// for FORTRAN bitcode linking since FORTRAN cannot use c headers.
-#ifdef __BUILD_MATH_BUILTINS_LIB__
-#include <limits.h>
-#define HUGE_VALF (__builtin_huge_valf())
-#define HUGE_VAL (__builtin_huge_val())
-#define __DEVICE__ extern __attribute__((always_inline, nothrow, cold, weak))
-#else
 #define __DEVICE__ static __attribute__((always_inline, nothrow))
-#endif // __BUILD_MATH_BUILTINS_LIB__
-#endif // __cplusplus
+#endif
 #else
-// CUDA Clang
 #define __DEVICE__ static __device__ __forceinline__
 #endif
 
