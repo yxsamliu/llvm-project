53f658e15e48b0c027078c1ebfacdbacf5f5ef47
Ron Lieberman <ron.lieberman@amd.com>
2021-02-25 16:27:42 -0600

    A+A compiler components selection based on aocc compiler options
    Adds support to invoke propietary source build of llvm compiler to provide
    propietary CPU optimizations combined wih HIP, OpenMP, OpenCL programming
    model support.
    
    Closed compiler lives in <installation>/alt/bin
    clang-NN and ld.lld are the components that are executed from <>/alt/bin
    
    -fclosed-opt enables invocation regardless of other opt flags.
    -fno-closed-opt disables invocation regardless of other opt flags.
    
    If the proprietary optimizations are selected, and the dir alt/ is not found
    then a warning is issued, and the open compiler toolchain is invoked.
    
    Future patch will add detection of aocc optimization flags to invoke
    the propietary compiler build.
    
    Change-Id: I88fbecca3bcd41803bbaf8cdc750d3e963d9b5b1

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -6194,6 +6194,8 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
                    options::OPT_fno_slp_vectorize, EnableSLPVec))
     CmdArgs.push_back("-vectorize-slp");
 
+  bool ProprietaryToolChainNeeded =
+    checkForAMDProprietaryOptOptions(TC, D, Args, CmdArgs);
   ParseMPreferVectorWidth(D, Args, CmdArgs);
 
   Args.AddLastArg(CmdArgs, options::OPT_fshow_overloads_EQ);
@@ -6341,8 +6343,12 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
 
   Args.AddAllArgs(CmdArgs, options::OPT_undef);
 
-  const char *Exec = D.getClangProgramPath();
+  std::string AltPath = D.getInstalledDir();
+  AltPath += "/../alt/bin/clang-" + std::to_string(LLVM_VERSION_MAJOR);
 
+  const char *Exec = ProprietaryToolChainNeeded
+         ? C.getArgs().MakeArgString(AltPath.c_str())
+	 : D.getClangProgramPath();
   // Optionally embed the -cc1 level arguments into the debug info or a
   // section, for build analysis.
   // Also record command line arguments into the debug info if
@@ -6634,7 +6640,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
       Input.getInputArg().renderAsInput(Args, CmdArgs);
   }
 
-  if (D.CC1Main && !D.CCGenDiagnostics) {
+  if (D.CC1Main && !D.CCGenDiagnostics && !ProprietaryToolChainNeeded) {
     // Invoke the CC1 directly in this process
     C.addCommand(std::make_unique<CC1Command>(JA, *this,
                                               ResponseFileSupport::AtFileUTF8(),
