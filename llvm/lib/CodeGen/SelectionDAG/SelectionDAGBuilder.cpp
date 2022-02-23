08678c65af8da5025c1d6569b1d06140e71fa5fe
Scott Linder <Scott.Linder@amd.com>
2022-02-23 20:01:04 +0000

    [HeterogeneousDwarf][SelectionDAG] Fix use-after-return
    This use-after-return only seems to trigger observable failures in some
    exceptional circumstances, so there is no practical test to accompany
    the patch.
    
    Fix amdgpu-debug-info-variable-expression-heterogeneous-dwarf.cl
    
    In some builds clang will default to eliding non-global value names in
    IR identifiers, which confused this test. Explicitly request that the
    identifier names not be elided.
    
    Change-Id: I7f1198fb1c70a86fae9d60bd388fb994d1af9e62

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -6136,7 +6136,7 @@ void SelectionDAGBuilder::visitIntrinsicCall(const CallInst &I,
           }
         }
       } else {
-        cast<SDDbgDef>(SDDK)->setSDValue(&N);
+        cast<SDDbgDef>(SDDK)->setSDValue(N);
       }
     } else {
       SDDK =
