569221be0c3086b88013c866044f3d5d1c6f4e6a
Ethan Stewart <ethan.stewart@amd.com>
2023-02-08 23:39:27 -0600

    [clang][driver] - Fix bug where the suffix logic is incorrect.
    If an archive such as libclang_rt.builtins-x86_64.a
    is encountered the current logic will split the
    string based on the first '.', which results in
    an archive being improperly turned into an object
    file. This patch properly gets the suffix in such
    scenarios and avoids an issue we are seeing
    in clang build select link during qmcpack build.
    This patch was needed for normal compilation as
    well as -save-temps.
    
    Change-Id: Ibf0f01e7e910d53c19b7c4a3b0e3cc459f7bf724

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -6007,15 +6007,16 @@ const char *Driver::GetNamedOutputPath(Compilation &C, const JobAction &JA,
        !C.getArgs().hasArg(options::OPT__SLASH_Fo)) ||
       CCGenDiagnostics) {
     StringRef Name = llvm::sys::path::filename(BaseInput);
-    std::pair<StringRef, StringRef> Split = Name.split('.');
-    SmallString<128> fname(Split.first.str().c_str());
+    size_t pos = Name.find_last_of(".");
+    StringRef PrefixName = Name.substr(0, pos);
+    SmallString<128> fname(PrefixName.str().c_str());
     if (!BoundArch.empty()) {
       fname += "-";
       fname.append(BoundArch);
     }
     SmallString<128> TmpName;
     const char *Suffix = nullptr;
-    if (Split.second == "a")
+    if (Name.ends_with(".a"))
       Suffix = "a";
     else
       Suffix = types::getTypeTempSuffix(JA.getType(), IsCLMode());
@@ -6035,11 +6036,11 @@ const char *Driver::GetNamedOutputPath(Compilation &C, const JobAction &JA,
       }
     } else {
       if (MultipleArchs && !BoundArch.empty()) {
-        TmpName = GetTemporaryDirectory(Split.first);
+        TmpName = GetTemporaryDirectory(PrefixName);
         llvm::sys::path::append(TmpName,
-                                Split.first + "-" + BoundArch + "." + Suffix);
+                                PrefixName + "-" + BoundArch + "." + Suffix);
       } else {
-        TmpName = GetTemporaryPath(Split.first, Suffix);
+        TmpName = GetTemporaryPath(PrefixName, Suffix);
       }
     }
     return C.addTempFile(C.getArgs().MakeArgString(TmpName));
@@ -6123,7 +6124,11 @@ const char *Driver::GetNamedOutputPath(Compilation &C, const JobAction &JA,
     NamedOutput =
         MakeCLOutputFilename(C.getArgs(), Val, BaseName, types::TY_Object);
   } else {
-    const char *Suffix = types::getTypeTempSuffix(JA.getType(), IsCLMode());
+    const char *Suffix = nullptr;
+    if (BaseName.ends_with(".a"))
+      Suffix = "a";
+    else
+      Suffix = types::getTypeTempSuffix(JA.getType(), IsCLMode());
     assert(Suffix && "All types used for output should have a suffix.");
 
     std::string::size_type End = std::string::npos;
@@ -6179,9 +6184,10 @@ const char *Driver::GetNamedOutputPath(Compilation &C, const JobAction &JA,
     // Must share the same path to conflict.
     if (SameFile) {
       StringRef Name = llvm::sys::path::filename(BaseInput);
-      std::pair<StringRef, StringRef> Split = Name.split('.');
+      size_t pos = Name.find_last_of(".");
+      StringRef PrefixName = Name.substr(0, pos);
       std::string TmpName = GetTemporaryPath(
-          Split.first, types::getTypeTempSuffix(JA.getType(), IsCLMode()));
+          PrefixName, types::getTypeTempSuffix(JA.getType(), IsCLMode()));
       return C.addTempFile(C.getArgs().MakeArgString(TmpName));
     }
   }
