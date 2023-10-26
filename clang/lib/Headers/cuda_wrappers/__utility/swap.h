8199b47f5b84c6bdac9ab373a4e138ad9246d033
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2023-10-26 15:09:49 -0400

    [CUDA][HIP] Fix std::is_invocable
    Currently std::is_invocable does not work for CUDA/HIP since
    its implementation requires checking whether a function
    is invocable in the context of a synthesized host function.
    
    In general, to make <type_traits> work with CUDA/HIP, the
    template functions need to be defined as
    so that they are available in both host and device contexts.
    
    Fixes: https://github.com/llvm/llvm-project/issues/69956
    
    Fixes: SWDEV-428314

---
+++ clang/lib/Headers/cuda_wrappers/__utility/swap.h
@@ -0,0 +1,3 @@
+#pragma clang force_cuda_host_device begin
+#include_next "__utility/swap.h"
+#pragma clang force_cuda_host_device end
---
+++ clang/lib/Headers/cuda_wrappers/bits/move.h
@@ -0,0 +1,3 @@
+#pragma clang force_cuda_host_device begin
+#include_next "bits/move.h"
+#pragma clang force_cuda_host_device end
