94979b8d1bc4ddcd47024a3966b07b1d31e31914
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2024-08-27 09:39:47 -0400

    [MC] output inlined-at debug info
    Currently MC print source location of instructions in comments
    in assembly when debug info is available, however, it does
    not include inlined-at locations when a function is inlined.
    
    For example, function foo is defined in header file a.h and
    is called multiple times in b.cpp. If foo is inlined,
    current assembly will only show its instructions with
    their line numbers in a.h. With inlined-at locations,
    the assembly will also show where foo is called in b.cpp.
    
    This patch adds inlined-at locations to the comments by
    using DebugLoc::print. It makes the printed source location
    info consistent with those printed by machine passes.
    
    Change-Id: I1138ce639e2e5a1e1ccddd3982cf651fb6a34433

--- llvm/lib/MC/MCObjectStreamer.cpp
+++ llvm/lib/MC/MCObjectStreamer.cpp
@@ -418,13 +418,14 @@ void MCObjectStreamer::emitDwarfLocDirective(unsigned FileNo, unsigned Line,
                                              unsigned Column, unsigned Flags,
                                              unsigned Isa,
                                              unsigned Discriminator,
-                                             StringRef FileName) {
+                                             StringRef FileName,
+                                             StringRef Location) {
   // In case we see two .loc directives in a row, make sure the
   // first one gets a line entry.
   MCDwarfLineEntry::make(this, getCurrentSectionOnly());
 
   this->MCStreamer::emitDwarfLocDirective(FileNo, Line, Column, Flags, Isa,
-                                          Discriminator, FileName);
+                                          Discriminator, FileName, Location);
 }
 
 static const MCExpr *buildSymbolDiff(MCObjectStreamer &OS, const MCSymbol *A,
