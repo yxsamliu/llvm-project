56190b69bbb0039b3e60d7ceadbbdd35c1a2a6b7
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2025-04-21 10:26:17 -0400

    add test
    Change-Id: Ice31537daade8a1b8ef981ed8db290b63bf86ce8

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
-DBG_LAMBDA=1 bin/clang++ -v -nogpuinc -nogpulib -fgpu-rdc -c lamba.hip 2>&1 | tee out.txt
+#DBG_LAMBDA=1 bin/clang++ -v -nogpuinc -nogpulib -fgpu-rdc -c lamba.hip 2>&1 | tee out.txt
+bin/clang++ -nogpuinc -nogpulib -fgpu-rdc -c -v -save-temps lamba.hip
+for a in lamba*.bc; do bin/llvm-dis $a; done
