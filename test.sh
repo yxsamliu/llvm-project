5871a8a0917f2dee16ebcfc7a2f4867f37c5e03b
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2025-07-29 02:41:28 +0530

    update test.sh
    use -fprofile-generate instead of -fprofile-instr-generate

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
-cd /home/yaxunl/git/llvm2
-ninja -C assert install || exit -1
+#cd /home/yaxunl/git/llvm2
+cd /c/git/llvm2
+
+#ninja -C assert install || exit -1
+PATH=/c/git/llvm2/install/bin:$PATH
+export ROCM_PATH=/c/hipsdk/hip.cpl.547
 
 export DB_PROF=1
 
@@ -8,7 +12,9 @@ export DB_PROF=1
 #assert/bin/clang++ -O2 --hip-path=/home/yaxunl/git/clr/Debug/install --offload-arch=gfx1100 -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
 #cp tmp_rovodev_hip_pgo_comprehensive_test-hip-amdgcn-amd-amdhsa-gfx1100.s tmp_rovodev_hip_pgo_comprehensive_test-hip-amdgcn-amd-amdhsa-gfx1100_orig.s
 
-assert/bin/clang++ -Wno-unused-value -O2 --hip-path=/home/yaxunl/git/clr/Debug/install -fprofile-instr-generate=my.profraw --offload-arch=gfx1100 -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
+#clang++ -Wno-unused-value -O2 --hip-path=/home/yaxunl/git/clr/Debug/install -fprofile-instr-generate=my.profraw --offload-arch=gfx1100 -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
+
+clang++ -mllvm -debug-only=pgo-instrumentation -Wno-unused-value -O2 -fprofile-generate=my.profraw --offload-arch=native -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
 
 #readelf -sW a.out-hip-amdgcn-amd-amdhsa-gfx1100 | grep __llvm_offload_prf
 #readelf -sW tmp_rovodev_hip_pgo_test_direct | grep __llvm_offload_prf
@@ -23,12 +29,13 @@ export LD_LIBRARY_PATH=/home/yaxunl/git/clr/Debug/install/lib
 #AMD_LOG_LEVEL=3 /opt/rocm/bin/rocgdb ./tmp_rovodev_hip_pgo_test_direct
 
 ls *.profraw
-#./assert/bin/llvm-profdata show my.amdgcn-amd-amdhsa.profraw --text --all-functions
-./assert/bin/llvm-profdata merge -o my.profdata my.profraw
-./assert/bin/llvm-profdata merge -o my.amdgcn-amd-amdhsa.profdata my.amdgcn-amd-amdhsa.profraw
+llvm-profdata show my.amdgcn-amd-amdhsa.profraw --text --all-functions
+llvm-profdata merge -o my.profdata my.profraw
+llvm-profdata merge -o my.amdgcn-amd-amdhsa.profdata my.amdgcn-amd-amdhsa.profraw
 
-assert/bin/clang++ -Wno-unused-value -O2 --hip-path=/home/yaxunl/git/clr/Debug/install -fprofile-instr-use=my.profdata --offload-arch=gfx1100 -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
+#assert/bin/clang++ -Wno-unused-value -O2 --hip-path=/home/yaxunl/git/clr/Debug/install -fprofile-instr-use=my.profdata --offload-arch=gfx1100 -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
+clang++ -mllvm -debug-only=pgo-instrumentation -Wno-unused-value -O2 -fprofile-use=my.profdata --offload-arch=native -save-temps -x hip tmp_rovodev_hip_pgo_comprehensive_test.hip -o tmp_rovodev_hip_pgo_test_direct
 
 #diff tmp_rovodev_hip_pgo_comprehensive_test-hip-amdgcn-amd-amdhsa-gfx1100_orig.s tmp_rovodev_hip_pgo_comprehensive_test-hip-amdgcn-amd-amdhsa-gfx1100.s
 
-./tmp_rovodev_hip_pgo_test_direct
+#./tmp_rovodev_hip_pgo_test_direct
