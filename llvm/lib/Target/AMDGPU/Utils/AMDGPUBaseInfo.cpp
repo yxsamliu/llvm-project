a638ad806320b807414c3d591617cb539282477a
CHEN, GUO <GUO.CHEN@amd.com>
2025-09-24 21:17:17 -0400

    [GFX13][True16] added missing fmac_f16_t16_gfx13 (#4211)
    added missing fmac_f16_t16_gfx13 to isMac check list
    
    Co-authored-by: guochen2 <guochen2@amd.com>

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.cpp
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.cpp
@@ -745,6 +745,7 @@ bool isMAC(unsigned Opc) {
          Opc == AMDGPU::V_FMAC_F16_fake16_e64_gfx11 ||
          Opc == AMDGPU::V_FMAC_F16_t16_e64_gfx12 ||
          Opc == AMDGPU::V_FMAC_F16_fake16_e64_gfx12 ||
+         Opc == AMDGPU::V_FMAC_F16_t16_e64_gfx13 ||
          Opc == AMDGPU::V_FMAC_F16_fake16_e64_gfx13 ||
          Opc == AMDGPU::V_DOT2C_F32_F16_e64_vi ||
          Opc == AMDGPU::V_DOT2C_F32_BF16_e64_vi ||
