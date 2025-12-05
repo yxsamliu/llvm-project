35ff45a86c8338aee691583e3318d954e3e9bd26
Brkusanin, Mirko <Mirko.Brkusanin@amd.com>
2025-12-05 12:43:14 +0100

    [AMDGPU][GFX1170] Disable f16 inline constants for pseudo scalar transcendentals (#4878)
    Similar as https://github.com/llvm/llvm-project/issues/104395.
    Simply enable for gfx1170 as well.

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -1447,7 +1447,7 @@ public:
   /// \returns true if inline constants are not supported for F16 pseudo
   /// scalar transcendentals.
   bool hasNoF16PseudoScalarTransInlineConstants() const {
-    return getGeneration() == GFX12;
+    return getGeneration() == GFX12 || isGFX1170();
   }
 
   /// \returns true if the target has instructions with xf32 format support.
