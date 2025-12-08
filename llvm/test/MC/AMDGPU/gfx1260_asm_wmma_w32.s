97b3c595b0215e9d97016427d34327cb28f74fb9
Tian, Shilei <Shilei.Tian@amd.com>
2025-12-08 14:17:59 -0500

    [AMDGPU][GFX1260] Add MC support for `v_wmma_f16_32x64x256_f4` (#4885)

--- llvm/test/MC/AMDGPU/gfx1260_asm_wmma_w32.s
+++ llvm/test/MC/AMDGPU/gfx1260_asm_wmma_w32.s
@@ -386,6 +386,26 @@ v_wmma_i32_32x64x32_iu8 v[0:63], v[64:71], v[72:87], 1
 // GFX1260: v_wmma_i32_32x64x32_iu8 v[0:63], v[64:71], v[72:87], 1 ; encoding: [0x00,0x00,0x90,0xcc,0x40,0x91,0x06,0x1a]
 // WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
 
+v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159]
+// GFX1260: v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] ; encoding: [0x00,0x40,0x9c,0xcc,0x40,0x81,0x03,0x1e]
+// WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
+
+v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] neg_lo:[1,0,0]
+// GFX1260: v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] neg_lo:[1,0,0] ; encoding: [0x00,0x40,0x9c,0xcc,0x40,0x81,0x03,0x3e]
+// WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
+
+v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] neg_lo:[0,1,0]
+// GFX1260: v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] neg_lo:[0,1,0] ; encoding: [0x00,0x40,0x9c,0xcc,0x40,0x81,0x03,0x5e]
+// WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
+
+v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] neg_lo:[0,0,1]
+// GFX1260: v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], v[128:159] neg_lo:[0,0,1] ; encoding: [0x00,0x40,0x9c,0xcc,0x40,0x81,0x03,0x9e]
+// WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
+
+v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], 1.0
+// GFX1260: v_wmma_f16_32x64x256_f4 v[0:31], v[64:95], v[192:255], 1.0 ; encoding: [0x00,0x40,0x9c,0xcc,0x40,0x81,0xcb,0x1b]
+// WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
+
 v_swmmac_bf16f32_32x64x32_bf16 v[192:255], v[0:7], v[8:39], v40
 // GFX1260: v_swmmac_bf16f32_32x64x32_bf16 v[192:255], v[0:7], v[8:39], v40 ; encoding: [0xc0,0x00,0xa1,0xcc,0x00,0x11,0xa2,0x1c]
 // WAVESIZE-ERR: :[[@LINE-2]]:1: error: instruction requires wavesize=32
