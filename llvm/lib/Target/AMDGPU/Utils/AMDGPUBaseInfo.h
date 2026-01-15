1de9c3cf45a0355b4a40e4d7e0aa01da02577ef2
Nash, Joseph <Joseph.Nash@amd.com>
2026-01-15 11:42:44 -0500

    [GFX13] Workaround dropped CMACC writes (#5037)
    Fixes [LWPSCGFX13-828](https://amd.atlassian.net/browse/LWPSCGFX13-828).
    Define a target feature for the bug, it affects all GFX13 A0 parts.
    Add tablgen table to list CMACC instructions.
    Define a hazard between CMACC instructions with the same dst register.

--- llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
+++ llvm/lib/Target/AMDGPU/Utils/AMDGPUBaseInfo.h
@@ -1020,6 +1020,9 @@ bool isInvalidSingleUseConsumerInst(unsigned Opc);
 LLVM_READONLY
 bool isInvalidSingleUseProducerInst(unsigned Opc);
 
+LLVM_READONLY
+bool isCMACCInstruction(unsigned Opc);
+
 bool isDPMACCInstruction(unsigned Opc);
 
 LLVM_READONLY
