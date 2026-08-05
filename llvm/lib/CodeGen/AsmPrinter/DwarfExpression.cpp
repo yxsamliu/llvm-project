c7bd1208f092b7c79934473d1ad8e2d98d5ac4fe
Ron Lieberman <ron.lieberman@amd.com>
2026-08-05 04:49:08 -0500

    Revert "[DebugInfo] Treat tag offsets as non-emitting (#211969)"
    defer, breaks spirv testing/dbg
    
    This reverts commit 32b645d9d69b8c2e62890fed19203337beddf797.

--- llvm/lib/CodeGen/AsmPrinter/DwarfExpression.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfExpression.cpp
@@ -28,19 +28,6 @@ using namespace llvm;
 
 #define DEBUG_TYPE "dwarfdebug"
 
-/// Return whether the rest of the expression needs the complex register path.
-/// We use this to decide whether we can emit a simple register location and
-/// whether a subregister needs to be masked. Non-emitting operations don't
-/// affect either decision, so look past them.
-static bool isRemainingExpressionComplex(const DIExpressionCursor &ExprCursor) {
-  for (DIExpression::ExprOperand Op : ExprCursor) {
-    if (Op.isNonEmitting())
-      continue;
-    return Op.getOp() != dwarf::DW_OP_LLVM_fragment;
-  }
-  return false;
-}
-
 void DwarfExpression::emitConstu(uint64_t Value) {
   if (Value < 32)
     emitOp(dwarf::DW_OP_lit0 + Value);
@@ -328,7 +315,10 @@ bool DwarfExpression::addMachineRegExpression(const TargetRegisterInfo &TRI,
     return false;
   }
 
-  bool HasComplexExpression = isRemainingExpressionComplex(ExprCursor);
+  bool HasComplexExpression = false;
+  auto Op = ExprCursor.peek();
+  if (Op && Op->getOp() != dwarf::DW_OP_LLVM_fragment)
+    HasComplexExpression = true;
 
   // If the register can only be described by a complex expression (i.e.,
   // multiple subregisters) it doesn't safely compose with another complex
@@ -378,7 +368,9 @@ bool DwarfExpression::addMachineRegExpression(const TargetRegisterInfo &TRI,
     DwarfRegs.clear();
     // If we need to mask out a subregister, do it now, unless the next
     // operation would emit an OpPiece anyway.
-    if (SubRegisterSizeInBits && isRemainingExpressionComplex(ExprCursor))
+    auto NextOp = ExprCursor.peek();
+    if (SubRegisterSizeInBits && NextOp &&
+        (NextOp->getOp() != dwarf::DW_OP_LLVM_fragment))
       maskSubRegister();
     return true;
   }
@@ -403,17 +395,6 @@ bool DwarfExpression::addMachineRegExpression(const TargetRegisterInfo &TRI,
     return false;
   }
 
-  // Consume leading tag offsets before matching the register expression.
-  // Record the tag offset here because addExpression won't see a consumed
-  // operation.
-  while (auto Op = ExprCursor.peek()) {
-    if (Op->getOp() != dwarf::DW_OP_LLVM_tag_offset)
-      break;
-    TagOffset = Op->getArg(0);
-    ExprCursor.take();
-  }
-
-  auto Op = ExprCursor.peek();
   auto Reg = DwarfRegs[0];
   int SignedOffset = 0;
 
@@ -465,7 +446,9 @@ bool DwarfExpression::addMachineRegExpression(const TargetRegisterInfo &TRI,
 
   // If we need to mask out a subregister, do it now, unless the next
   // operation would emit an OpPiece anyway.
-  if (SubRegisterSizeInBits && isRemainingExpressionComplex(ExprCursor))
+  auto NextOp = ExprCursor.peek();
+  if (SubRegisterSizeInBits && NextOp &&
+      (NextOp->getOp() != dwarf::DW_OP_LLVM_fragment))
     maskSubRegister();
 
   return true;
