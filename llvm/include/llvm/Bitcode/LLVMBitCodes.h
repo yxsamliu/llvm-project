d6edaba0220368a89082b0d5ff9db2d90802e04b
Scott Linder <scott.linder@amd.com>
2026-08-14 18:34:15 -0400

    Restore upstream metadata bitcode values (#3922)
    When initially committed, the change to add downstream-only bitcode
    encodings for DIExpr/DIFragment/DILifetime should have used much larger
    values (the approach in other similar situations is either to start
    allocating backwards from near the end of the underlying integral type,
    or to just set some higher bit and allocate sequentially starting
    there).
    
    Since I didn't do that initially, we ended up in a precarious situation
    where merges of new upstream codes needed to break our downstream codes.
    At some point `METADATA_SUBRANGE_TYPE` and `METADATA_FIXED_POINT_TYPE`
    were merged with broken codes, taken sequentially after our
    downstream-only ones.
    
    This patch restores the upstream codes, and so breaks any bitcode
    generated with amd-staging that uses these metadata kinds. I don't think
    there is any reasonable path to avoiding that breakage, since there is
    no other context to "downgrade" codes 51 and 52 without causing the same
    issue for them in the future.
    
    We could temporarily do the downgrade, and after some period drop it
    from amd-staging, but I am concerned with the difficulty in ensuring we
    do that in a timely manner.
    
    Change-Id: I45c6043310d611bbe021a4bece442acfb380847c

--- llvm/include/llvm/Bitcode/LLVMBitCodes.h
+++ llvm/include/llvm/Bitcode/LLVMBitCodes.h
@@ -398,11 +398,8 @@ enum MetadataCodes {
   METADATA_GENERIC_SUBRANGE = 45, // [distinct, count, lo, up, stride]
   METADATA_ARG_LIST = 46,         // [n x [type num, value num]]
   METADATA_ASSIGN_ID = 47,        // [distinct, ...]
-  METADATA_EXPR = 48,             // [distinct, ...]
-  METADATA_FRAGMENT = 49,         // []
-  METADATA_LIFETIME = 50,         // [obj, loc, n x args]
-  METADATA_SUBRANGE_TYPE = 51,    // [distinct, ...]
-  METADATA_FIXED_POINT_TYPE = 52, // [distinct, ...]
+  METADATA_SUBRANGE_TYPE = 48,    // [distinct, ...]
+  METADATA_FIXED_POINT_TYPE = 49, // [distinct, ...]
 };
 
 // The constants block (CONSTANTS_BLOCK_ID) describes emission for each
